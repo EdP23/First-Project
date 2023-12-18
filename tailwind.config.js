0a9eb282176f71b3b1a9c9c375d5a1e8abfb2738
EdP23 <edgars.petrackovs@dci-student.org>
2023-12-18 18:35:30 +0100

    day4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./dist/**/*.{html,js}" ],
+  content: ["./dist/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
     extend: {
       fontFamily: {
-        brawler: ["'Brawler'", 'sans-serif'],
+brawler:"'Brawler', sans-serif",
+rubik:"'Rubik', sans-serif",
       },
     },
+    // FontSize
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+  },
   plugins: [],
-}
-}
+};
