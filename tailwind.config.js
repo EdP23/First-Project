80996080c018f25366250f2b8a1a7d7ae5a11a97
EdP23 <edgars.petrackovs@dci-student.org>
2023-12-21 16:17:58 +0100

    day6

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-brawler:"'Brawler', sans-serif",
-rubik:"'Rubik', sans-serif",
+        brawler: "'Brawler', sans-serif",
+        rubik: "'Rubik', sans-serif",
       },
     },
     // FontSize
@@ -18,6 +18,26 @@ rubik:"'Rubik', sans-serif",
       "3xl": "1.953rem",
       "4xl": "2.441rem",
       "5xl": "3.052rem",
+    },
+    //Screens
+    screens: {
+      xs: { min: "380px", max: "523px" },
+      // => @media (min-width: 380px and max-width: 523px) { ... }
+
+      sm: { min: "523px", max: "767px" },
+      // => @media (min-width: 523px and max-width: 767px) { ... }
+
+      md: { min: "768px", max: "1023px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px", max: "1279px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1280px", max: "1535px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1536px" },
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
